ba9df683dbbbfa19deb44e44103ec2fe0adf55d7
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-10 16:15:32 -0600

    Completed DELETE task

--- api/server.js
+++ api/server.js
@@ -45,4 +45,18 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await User.remove(id);
+        if (!deletedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.json(deletedUser);
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user could not be removed" });
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
