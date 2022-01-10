c8973d754fe3e0fb895458bd4a554328b6e20c12
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-10 16:56:09 -0600

    Completed UPDATE user task - MVP finished

--- README.md
+++ README.md
@@ -29,7 +29,9 @@ Add the code necessary in `index.js` and `api/server.js` to create a Web API and
 | GET    | /api/users     | Returns an array users. |
 
 | GET    | /api/users/:id | Returns the user object with the specified `id`. |
+
 | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user. |
+
 | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
 
 #### User Schema
--- api/server.js
+++ api/server.js
@@ -59,4 +59,23 @@ server.delete('/api/users/:id', async (req, res) => {
     }
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, bio } = req.body;
+        if ( !name || !bio ) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            const updatedUser = await User.update(id, { name, bio });
+            if (!updatedUser) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.status(200).json(updatedUser);
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user information could not be modified" });
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
