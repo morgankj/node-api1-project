09deb5ee6fd52b9a9a76dd3ceefb1c3d5baa6e5b
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-10 16:05:39 -0600

    Completed task to GET user by ID

--- api/server.js
+++ api/server.js
@@ -31,4 +31,18 @@ server.get('/api/users', async (req, res) => {
     }
 })
 
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user information could not be retrieved" });
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
