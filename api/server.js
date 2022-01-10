bf967f7c9bcfd7cf79aad98354b1cd6fe25cc679
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-10 15:45:16 -0600

    Completed post request

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model.js');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', async (req, res) => {
+    const { name, bio } = req.body;
+    try {
+        if (!name || !bio) {
+            res.status(404).json({ message: "Please provide name and bio for the user" })
+        } else {
+            const newUser = await User.insert({ name, bio });
+            console.log("New user created: ", newUser);
+            res.status(200).json(newUser);
+        }
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
